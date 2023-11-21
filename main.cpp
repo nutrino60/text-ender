1c94a9b6391d800c45581f635432bb2d9e605a2f
nutrino60 <151408441+nutrino60@users.noreply.github.com>
2023-11-21 13:02:49 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "text_render/render.h"
+#include <string>
+#include <string>
+
+
+int main()
+{
+    Drawline(10,'h');
+
+
+    repeat("",99 ,'f');
+
+    DrawRec(4,12,' ','#');
+
+}
